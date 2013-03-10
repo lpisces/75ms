ca2af6c6ce2bebf4c54a50193e1c668396823c7c
Mingming Tang <lpisces@vr.75ms.com>
2013-03-10 04:22:24 -0700

    remove user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130309152753) do
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "password_digest"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+ActiveRecord::Schema.define(:version => 0) do
 
 end
